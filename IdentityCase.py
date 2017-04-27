f52b5223b6b6c2aa9fccc53e3f12ce2bfd08ea20
Mitch Perry <moperry00@gmail.com>
2017-04-27 17:59:46 -0400

    Finished identity case/test

---
+++ IdentityCase.py
@@ -0,0 +1,242 @@
+import cvxpy
+import numpy
+import math
+
+def Checker(A, b, c, epsilon):
+    """ Checks to see if the user-defined input is of the correct form.
+        We require that A be a list of m (nxn)-dimensional numpy matrices
+        (with m >= 1), b be an (m x 1)-dimensional numpy matrix, c be an (n x n)-dimensional
+        numpy matrix, and epsilon be a number between 0 and 1 (not inclusive).
+    """
+    
+    if (not isinstance(epsilon, float)) or (epsilon >= 1) or (epsilon <= 0):
+        raise ValueError('epsilon must be a number between 0 and 1 (not inclusive).')
+
+    if (not isinstance(c, numpy.matrix)) or (c.shape[0] != c.shape[1]):
+        raise ValueError('c must be a square numpy matrix.')
+
+    if (not isinstance(b, numpy.matrix)) or (b.shape[1] != 1):
+        raise ValueError('b must be an (m x 1)-dimensional numpy matrix.')
+
+    if (not isinstance(A, list)) or (len(A) == 0):
+        raise ValueError('A must be a list that includes at least one constraint.')
+
+    for i in range(0, len(A)):
+        if (not isinstance(A[i], numpy.matrix)) or (A[i].shape[0] != A[i].shape[1]):
+            raise ValueError('Each element of A must be a square numpy matrix.')
+
+    # Now we go through and make sure dimensions all match.
+    n = c.shape[0]
+    m = b.shape[0]
+
+    if (len(A) != m):
+        raise ValueError('Number of constraints in A should match the length of b.')
+
+    for i in range(0, len(A)):
+        if (A[i].shape[0] != n):
+            raise ValueError('Dimensions of each matrix in A must match dimensions of c.')
+
+    return [m, n]
+
+
+def InitialFinder(A, b, c, z):
+    """ Input: A is a list of m (n x n)-dimensional numpy matrices, b is
+               an (m x 1)-dimensional numpy matrix, c is an
+               (n x n)-dimensional numpy matrix, and z is a number that is
+               less than the inner product of c and the identity matrix.
+        Output: Returns an (n x n)-dimensional symmetric matrix that is in
+                the affine space specified by the original problem CP (see page 3
+                of paper) and whose objective function value is equal to z.
+        Description: We use cvxpy to find a starting point for the supgradient algorithm
+                     (see page 14 of paper).
+    """
+    
+    n = c.shape[0]
+    x = cvxpy.Variable(n, n)
+    objective = cvxpy.Minimize(0)
+    constraints = [cvxpy.trace(c.T * x) == z, x == x.T]
+    for i in range(0, len(A)):
+        constraints += [cvxpy.trace(A[i].T * x) == b[i]]
+    prob = cvxpy.Problem(objective, constraints)
+    result = prob.solve()
+    
+    if (prob.status == 'infeasible'):
+        raise ValueError('Problem Is Infeasible')
+    
+    if (prob.status == 'unbounded'):
+        raise ValueError('Problem Is Unbounded')
+
+    return x.value
+
+
+def Supgradient(x):
+    """ Input: x must be an (n x n)-dimensional numpy matrix that's a feasible
+               solution to problem (2.5) for some valid value of z (see page 4 of paper).
+               We're assuming that the distinguished direction e is the identity matrix.
+        Output: Returns an (n x n)-dimensional numpy matrix that represents
+                a supgradient of lambda_min(x) evaluated at the point x given.
+        Description: Calculates the supgradient that will be used to update our
+                     guess of the solution at each iteration of the main algorithm.
+                     See page 5 of the paper for specifics. This function assumes that the
+                     distinguished direction e is the identity matrix.
+    """
+    
+    eig_values, eig_vectors = numpy.linalg.eig(x)
+    sort_permutations = eig_values.argsort()
+    
+    # Sort eigenvalues and re-arrange eigenvectors to match up with eigenvalues.
+    eig_values.sort()
+    eig_vectors = eig_vectors[:, sort_permutations]
+    
+    v = eig_vectors[:, 0]
+    return v * numpy.transpose(v)
+
+
+def SupgradProjection(s, A, c):
+    """ Input: s is an (n x n)-dimensional numpy matrix that represents a
+               supgradient of lambda_min(x) for some x. A must be a list of
+               (n x n)-dimensional numpy matrices that corresponds to the affine
+               constraint in the original problem CP (see page 3 of paper),
+               and c must be an (n x n)-dimensional numpy matrix that
+               corresponds to the objective function in the original problem CP.
+        Output: Returns an (n x n)-dimensional numpy matrix that's the
+                projection of the supgradient s onto the intersection of the
+                null spaces of the matrices in A and the matrix c.
+        Description: Projects the supgradient s onto the intersection of the
+                     null spaces of the matrices in A with the null space
+                     of the matrix c (see page 5 of the paper).
+    """
+
+    sup_vec = numpy.matrix(numpy.ravel(s)).T
+    c_vec = numpy.matrix(numpy.ravel(c))
+    A_mat = numpy.matrix(numpy.ravel(A[0]))
+
+    for i in range(1, len(A)):
+        A_i = A[i]
+        A_i_vec = numpy.matrix(numpy.ravel(A_i))
+        A_mat = numpy.concatenate((A_mat, A_i_vec))
+
+    A_mat = numpy.concatenate((A_mat, c_vec))
+
+    # Compute projection matrix for null space of A_mat
+    n_2 = len(sup_vec)
+    n = int(math.sqrt(n_2))
+    I = numpy.matrix(numpy.identity(n_2))
+    P = I - A_mat.T * numpy.linalg.inv(A_mat * A_mat.T) * A_mat
+
+    # Projected original supgradient onto null space of A_mat and reshape into
+    # an (n x n)-dimensional matrix.
+    new_sup = P * sup_vec
+
+    to_return = new_sup[0:n].T
+    for i in range(1, n):
+        to_return = numpy.concatenate((to_return, new_sup[i*n:(i+1)*n].T))
+
+    return to_return
+
+
+def ModToOrig(x):
+    """ Input: x is an (n x n)-dimensional numpy matrix that corresponds to a
+               feasible solution to the modified problem (2.5) (see pages 3-4 of
+               the paper).
+        Output: An (n x n)-dimensional numpy matrix that corresponds to a
+                feasible solution to the original problem, assuming that the
+                distinguished direction used is the identity matrix.
+        Description: ModToOrig maps the point x to the boundary of the feasible
+                     region of the original problem. It corresponds to pi(x) in
+                     the paper (see page 3). 
+    """
+
+    n = x.shape[0]
+    e = numpy.identity(n)
+    eig_values, eig_vectors = numpy.linalg.eig(x)
+    eig_values_sorted = numpy.sort(eig_values)
+    smallest_eig = eig_values_sorted[0]
+
+    return e + (1 / (1 - smallest_eig)) * (x - e)
+
+
+def AlgoMain(A, c, x, z, eps):
+    """ Input: A must be a list of (n x n)-dimensional numpy matrices that
+               correspond to the affine constraints in the original problem CP
+               (see page 3), and c must be an (n x n)-dimensional numpy matrix
+               that corresponds to the objective function in the original
+               problem CP. x is an (n x n)-dimensional numpy matrix that's a
+               feasible solution to the modified problem (see page 4). z is a
+               number less than tr((c^T)I). Finally, eps is the desired
+               relative accuracy of the solution and must be between 0 and 1
+               (not inclusive).
+        Output: An (n x n)-dimensional numpy matrix that represents the optimal
+                solution to the original problem CP, along with this solution's
+                value.
+        Description: AlgoMain uses the supgradient projection algorithm described
+                     on page 14 to find an approximate solution to the original
+                     problem CP (see page 3). By "approximate solution" we mean
+                     a solution pi* that satisfies (<c, pi*> - z*) / (<c, e> - z*) <= eps,
+                     where z* is the true optimal value of the original problem CP.
+    """
+
+    # Initialize
+    n = x.shape[0]
+    e = numpy.identity(n)
+
+    x_0 = ModToOrig(x)
+    pi_0 = ModToOrig(x)
+
+    x_k = [x_0]
+    pi_k = [pi_0]
+
+    # Compute Iteration Bound
+    l = 1 / eps**3
+    l = int(numpy.ceil(l))
+
+    # Iterate
+    for i in range(0, l):
+        supgrad_k = Supgradient(x_k[i])
+        project_supgrad_k = SupgradProjection(supgrad_k, A, c)
+        x_tild = x_k[i] + (eps / 2) * (1 / numpy.linalg.norm(project_supgrad_k)) * project_supgrad_k
+        pi_k.append(ModToOrig(x_tild))
+
+        if numpy.trace(c.T * (e - pi_k[i+1])) >= (4/3) * numpy.trace(c.T * (e - x_tild)):
+            x_k.append(pi_k[i+1])
+        else:
+            x_k.append(x_tild)
+
+    opt_sol = pi_k[l]
+    opt_sol_val = numpy.trace(c.T * opt_sol)
+
+    return [opt_sol, opt_sol_val]
+
+
+def RenegarIdentitySDP(A, b, c, eps):
+    """ Input: A must be a list of (n x n)-dimensional numpy matrices that
+               correspond to the affine constraints in the original problem CP
+               (see page 3), and c must be an (n x n)-dimensional numpy matrix
+               that corresponds to the objecive function in the original
+               problem CP, and b is an (m x 1)-dimensional numpy matrix that
+               represents the RHS of the affine constraints in CP. eps is a
+               number that represents the desired accuracy of the returned
+               solution relative to the actual solution.
+        Output: A list with two elements: the first is an (n x n)-dimensional
+                numpy matrix that's a solution to the original problem CP, and
+                the second is the objective function value of this solution.
+        Description: We use the algorithm described in Renegar's paper to solve
+                     the following SDP: minimize <c, x> subject to Ax = b and
+                     x is in the cone of positive semidefinite matrices.
+    """
+
+    # First, check to see that the input given is valid.
+    m, n = Checker(A, b, c, eps)
+
+    # Now, find a starting point for the supgradient algorithm.
+    n = c.shape[0]
+    e_value = numpy.trace(c.T * numpy.identity(n))
+    z = e_value - .9 * abs(e_value)
+    initial_guess = InitialFinder(A, b, c, z)
+
+    # Finally, run supgradient algorithm and return optimal solution along
+    # with its value.
+
+    opt_sol, opt_sol_val = AlgoMain(A, c, initial_guess, z, eps)
+
+    return [opt_sol, opt_sol_val]
---
+++ IdentityTesting.py
@@ -0,0 +1,316 @@
+from IdentityCase import *
+import math
+import numpy
+import cvxpy
+
+# isclose is used to check if two floating point numbers are close
+# enough to be considered equal.
+def isclose(a, b, rel_tol, abs_tol):
+    return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
+
+# check_symmetric checks to see if the numpy matrix a is symmetric.
+def check_symmetric(a, tol = 1e-6):
+	return numpy.allclose(a, a.T, atol = tol)
+
+################################################################################
+################################ Test Checker ##################################
+ 
+print "Testing Checker"
+
+# Example where epsilon is not a float
+A = [numpy.matrix([[1, 2], [3, 4]])]
+c = numpy.matrix([[1, 2], [3, 4]])
+b = numpy.matrix([[1]])
+eps = "epsilon"
+
+try:
+	m, n = Checker(A, b, c, eps)
+except ValueError:
+	print "Checked that eps must be a number"
+
+# Example where epsilon is less than or equal to 0
+eps = -0.0
+
+try:
+	m, n = Checker(A, b, c, eps)
+except ValueError:
+	print "Checked that eps must be greater than 0"
+
+# Example where epsilon is greater than or equal to 1
+eps = 1.0
+
+try:
+	m, n = Checker(A, b, c, eps)
+except ValueError:
+	print "Checked that eps must be less that 1"
+
+# Example where c is not a matrix
+eps = 0.5
+c = 10
+
+try:
+	m, n = Checker(A, b, c, eps)
+except ValueError:
+	print "Checked that c must be a matrix"
+
+# Example where c is not symmetric
+c = numpy.matrix([[1, 2, 3], [4, 5, 6]])
+
+try:
+	m, n = Checker(A, b, c, eps)
+except ValueError:
+	print "Checked that c must be symmetric"
+
+# Example where b is not a matrix
+c = numpy.matrix([[1, 2], [3, 4]])
+b = 10
+
+try:
+	m, n = Checker(A, b, c, eps)
+except ValueError:
+	print "Checked that b must be a matrix"
+
+# Example where b has more than one column
+b = numpy.matrix([[1, 2], [3, 4]])
+
+try:
+	m, n = Checker(A, b, c, eps)
+except ValueError:
+	print "Checked that b must be a column vector"
+
+# Example where A is not a list
+b = numpy.matrix([[1]])
+A = numpy.matrix([[1, 2], [3, 4]])
+
+try:
+	m, n = Checker(A, b, c, eps)
+except ValueError:
+	print "Checked that A must be a list"
+
+# Example where A is an empty list
+A = []
+
+try:
+	m, n = Checker(A, b, c, eps)
+except ValueError:
+	print "Checked that A can't be an empty list"
+
+# Example where elements of A aren't matrices
+A = [10]
+
+try:
+	m, n = Checker(A, b, c, eps)
+except ValueError:
+	print "Checked that elements of A must be matrices"
+
+# Example where elements of A aren't symmetric
+A = [numpy.matrix([[1, 2, 3], [4, 5, 6]])]
+
+try:
+	m, n = Checker(A, b, c, eps)
+except ValueError:
+	print "Checked that elements of A must be symmetric matrices"
+
+# Example where length of A doesn't equal number of entries in b
+A = [numpy.matrix([[1, 2], [3, 4]]), numpy.matrix([[1, 2], [3, 4]])]
+
+try:
+	m, n = Checker(A, b, c, eps)
+except ValueError:
+	print "Checked that length of A must equal number of entries in b"
+
+# Example where elements of A aren't all the same dimension of c
+A = [numpy.matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])]
+
+try:
+	m, n = Checker(A, b, c, eps)
+except:
+	print "Checked that dimension of matrices in A must have same dimension as c"
+
+# Example where everything works out
+A = [numpy.matrix([[1, 2], [3, 4]]), numpy.matrix([[1, 2], [3, 4]])]
+c = numpy.matrix([[1, 2], [3, 4]])
+b = numpy.matrix([[1], [2]])
+eps = 0.5
+
+m, n = Checker(A, b, c, eps)
+
+print "Checked that everything works for correct input"
+
+print "Finished testing Checker"
+
+
+################################################################################
+############################# Test InitialFinder ###############################
+
+print "Testing InitialFinder"
+
+# Example where problem is infeasible
+A = [numpy.matrix([[1, 2], [3, 4]]), numpy.matrix([[1, 2], [3, 4]])]
+b = numpy.matrix([[0], [5]])
+c = numpy.matrix([[1, 2], [3, 4]])
+
+try:
+	x = InitialFinder(A, b, c, 5)
+except:
+	print "Checked that problem must be feasible"
+
+# Example where problem is unbounded
+A = [numpy.matrix([[1, 0], [0, -1]])]
+b = numpy.matrix([[0]])
+c = numpy.matrix([[-1, 0], [0, -1]])
+
+try:
+	x = InitialFinder(A, b, c, -2)
+except:
+	print "Checked that problem must be bouded"
+
+# Example where problem has a feasible solution
+A = [numpy.matrix([[1, 0], [0, -1]])]
+b = numpy.matrix([[0]])
+c = numpy.matrix([[1, 0], [0, 1]])
+
+x = InitialFinder(A, b, c, 1.5)
+
+# Make sure that x is symmetric, satisfies the affine constraint, 
+# and has objective function value equal to 1.5.
+
+if isclose(numpy.trace(A[0].T * x), b.item(0, 0), 1e-6, 0.01) and isclose(numpy.trace(c.T * x), 1.5, 1e-6, 0.01) and check_symmetric(x):
+	print "Checked that InitialFinder works for correct input"
+else:
+	raise ValueError("x isn't actually a solution to the desired problem.")
+
+print "Finished testing InitialFinder"
+
+
+
+if isclose(numpy.trace(A[0].T * x), b.item(0, 0), 1e-6, 0.01) and numpy.trace(c.T * x) <= numpy.trace(c.T * numpy.identity(c.shape[0])):
+	print "Checked that InitialFinder works for correct input"
+
+
+################################################################################
+############################## Test Supgradient ################################
+
+print "Testing Supgradient"
+
+# Assuming inputted x is a positive semi-definite matrix, check to make sure
+# that matrix returned by Supgradient function is what we expect.
+x = numpy.matrix([[2, -1, 0], [-1, 2, -1], [0, -1, 2]])
+supgrad_to_check = Supgradient(x)
+expected_supgrad = numpy.matrix([[.25, 1/(2*math.sqrt(2)), .25], [1/(2*math.sqrt(2)), .5, 1/(2*math.sqrt(2))], [.25, 1/(2*math.sqrt(2)), .25]])
+result = True
+for i in range(0, 3):
+    for j in range(0, 3):
+        if not isclose(supgrad_to_check.item((i, j)), expected_supgrad.item((i, j)), 1e-6, .01):
+            result = False
+if result:
+    print "Supgradient function works for x with distinct eigvalues and eigenvectors"
+else:
+    raise ValueError("Value returned by supgrad_to_check is not supgradient lambda_min at x.")
+
+# Example where x has repeated eigenvalues.
+x = numpy.matrix([[7, 0, -3], [-9, -2, 3], [18, 0, -8]])
+supgrad_to_check = Supgradient(x)
+expected_supgrad = numpy.matrix([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
+result = True
+for i in range(0, 3):
+    for j in range(0, 3):
+        if not isclose(supgrad_to_check.item((i, j)), expected_supgrad.item((i, j)), 1e-6, .01):
+            result = False
+if result:
+    print "Supgradient function works for x with repeated eigenvalues"
+else:
+    raise ValueError("Value returned by Supgrad function is not supgradient of lambda_min at x.")
+
+print "Finished testing Supgradient"
+
+
+################################################################################
+########################### Test SupgradProjection #############################
+
+print "Testing SupgradProjection"
+
+# Testing for specific instance of problem.  
+A = [numpy.matrix([[1, 1, 0], [1, -1, -1], [0, -1, 0]]), numpy.matrix([[1, 2, 0], [2, 1, 2], [0, -3, 1]])]
+c = numpy.matrix([[0, 1, 1], [1, 0, 1], [1, 1, 0]])
+supgradient = numpy.matrix([[.25, 1/(2*math.sqrt(2)), .25], [1/(2*math.sqrt(2)), .5, 1/(2*math.sqrt(2))], [.25, 1/(2*math.sqrt(2)), .25]])
+
+projected_supgrad_to_check = SupgradProjection(supgradient, A, c)
+expected_projected_supgrad = numpy.matrix([[0.27974, 0.02848, -.03326], [0.02848, 0.32714, -0.17412], [-.03326, 0.18367, 0.17844]])
+result = True
+
+for i in range(0, 3):
+	for j in range(0, 3):
+		if not isclose(projected_supgrad_to_check.item((i, j)), expected_projected_supgrad.item((i, j)), 1e-6, .01):
+			result = False
+
+if result:
+	print "Supgradient projection seems to work"
+else:
+	raise ValueError("Value returned by SupgradProjection function is not what we expect to see")
+
+print "Finished testing SupgradProjection"
+
+
+################################################################################
+################################ Test AlgoMain #################################
+
+print "Testing AlgoMain"
+
+# Test on sample problem whose solution we know from using cvxpy to solve it.
+A = [numpy.matrix([[1, 0, 1], [0, 3, 7], [1, 7, 5]]), numpy.matrix([[0, 2, 8], [2, 6, 0], [8, 0, 4]])]
+c = numpy.matrix([[1, 2, 3], [2, 9, 0], [3, 0, 7]])
+b = numpy.matrix([[9], [10]])
+z = 15
+initial_guess = InitialFinder(A, b, c, z)
+eps = 0.1
+
+# Used CVX in Matlab to find the value of the actual optimal solution.
+true_solution_value = 8.92
+
+AlgoMainSol, AlgoMainSolVal = AlgoMain(A, c, initial_guess, z, .1)
+
+if isclose(true_solution_value, AlgoMainSolVal, 1e-1, 0.01):
+	print "AlgoMain seems to get correct value"
+else:
+	raise ValueError("Value returned by AlgoMain function is not what we expect to see")
+
+eig_values, eig_vectors = numpy.linalg.eig(AlgoMainSol)
+smallest_eigenvalue = round(numpy.amin(eig_values), 6)
+
+if isclose(numpy.trace(A[0].T * AlgoMainSol), b[0], 1e-1, .01) and isclose(numpy.trace(A[1].T * AlgoMainSol), b[1], 1e-1, .01) and check_symmetric(AlgoMainSol) and smallest_eigenvalue >= 0:
+	print "AlgoMain gets a feasible solution"
+else:
+	raise ValueError("Solution returned by AlgoMain function violates constraints i.e. isn't feasible")
+
+print "Finished testing AlgoMain"
+
+
+################################################################################
+########################### Test RenegarIdentitySDP ############################
+
+print "Testing RenegarIdentitySDP"
+
+# Test on sample problem whose solution we know from using CVX to solve in Matlab.
+A = [numpy.matrix([[1, 0, 1], [0, 3, 7], [1, 7, 5]]), numpy.matrix([[0, 2, 8], [2, 6, 0], [8, 0, 4]])]
+c = numpy.matrix([[1, 2, 3], [2, 9, 0], [3, 0, 7]])
+b = numpy.matrix([[9], [10]])
+eps = 0.1
+
+solution, solution_value = RenegarIdentitySDP(A, b, c, eps)
+true_solution_value = 8.92
+
+if isclose(true_solution_value, solution_value, 1e-1, 0.01):
+	print "RenegarIdentitySDP seems to get correct value"
+else:
+	raise ValueError("Value returned by RenegarIdentitySDP function is not what we expect to see")
+
+eig_values, eig_vectors = numpy.linalg.eig(solution)
+smallest_eigenvalue = round(numpy.amin(eig_values), 6)
+
+if isclose(numpy.trace(A[0].T * solution), b[0], 1e-1, .01) and isclose(numpy.trace(A[1].T * solution), b[1], 1e-1, .01) and check_symmetric(solution) and smallest_eigenvalue >= 0:
+	print "RenegarIdentitySDP gets a feasible solution"
+else:
+	raise ValueError("Solution returned by RenegarIdentitySDP function violates constraints i.e. isn't feasible")
+
+print "Finished testing RenegarIdentitySDP"
